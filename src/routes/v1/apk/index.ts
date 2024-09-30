64736b566bd45b818b3c8ecc7320379bc93fa92e
Flandia Yingman <me@flandia.dev>
2024-09-30 20:06:43 +0800

    feat(apk): add version tracking

--- src/routes/v1/apk/index.ts
+++ src/routes/v1/apk/index.ts
@@ -40,6 +40,29 @@ const apk: FastifyPluginAsync<ApkOptions> = async (
     },
   );
 
+  fastify.get(
+    "/latest/version",
+    {
+      schema: {
+        summary: "Get the Latest USThing APK Version",
+        tags: ["APK"],
+        response: {
+          200: Type.String(),
+        },
+      },
+    },
+    async function () {
+      const latest = await fastify.mongo
+        .db!.collection("apk.version")
+        .findOne();
+      if (latest) {
+        return latest.version;
+      } else {
+        return "0.0.0";
+      }
+    },
+  );
+
   // Register to keep the Bearer Auth & Multipart to this scope
   fastify.register(async function (fastify) {
     fastify.register(import("@fastify/bearer-auth"), {
@@ -92,7 +115,26 @@ const apk: FastifyPluginAsync<ApkOptions> = async (
         }
       },
     );
+
+    fastify.post(
+      "/latest/version",
+      {
+        schema: {
+          summary: "Update the Latest USThing APK Version",
+          tags: ["APK"],
+          body: Type.String(),
+          response: {
+            200: Type.String(),
+          },
+          security: [{ ApkAuth: [] }],
+        },
+      },
+      async function (request) {
+        await fastify.mongo
+          .db!.collection("apk.version")
+          .updateOne({}, { $set: { version: request.body } }, { upsert: true });
+      },
+    );
   });
 };
-
 export default apk;
