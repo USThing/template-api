ca15e6d20b72c3f7ac1ed8194b44f1aaa70d96c2
Flandia Yingman <me@flandia.dev>
2024-11-08 16:20:30 +0800

    feat(course-catalog): search support

--- src/routes/v1/course-catalog/index.ts
+++ src/routes/v1/course-catalog/index.ts
@@ -6,15 +6,27 @@ import { Course } from "../../../plugins/course-catalog/types.js";
 const courseCatalog: FastifyPluginAsync = async (
   fastify: FastifyTypebox,
 ): Promise<void> => {
-  // Get all courses; if termCode is not specified, use the latest
+  const collection = fastify.mongo.db!.collection<Course>("course-catalog");
+
+  async function latestTermCode(): Promise<string> {
+    const codes = await collection.distinct("termCode", {});
+    return String(Math.max(...codes.map((it) => Number(it))));
+  }
+
+  // Get all courses, optionally filtered by a search string of courseCode or courseTitle;
+  // if termCode is not specified, use the latest
   fastify.get(
     "/",
     {
       schema: {
-        summary: "Get all courses.",
+        summary:
+          "Get all courses, optionally filtered by a search string of courseCode AND courseTitle.",
+        description: "If termCode is not specified, use the latest term.",
         tags: ["Course Catalog"],
         querystring: Type.Object({
           termCode: Type.Optional(Type.String()),
+          searchCode: Type.Optional(Type.String()),
+          searchTitle: Type.Optional(Type.String()),
         }),
         response: {
           200: Type.Array(Course),
@@ -23,19 +35,25 @@ const courseCatalog: FastifyPluginAsync = async (
     },
     async function (request) {
       const termCode = request.query.termCode ?? (await latestTermCode());
-      return await fastify.mongo
-        .db!.collection("course-catalog")
-        .find<Course>({ termCode }, { projection: { _id: 0 } })
+      const searchCode = request.query.searchCode ?? "";
+      const searchTitle = request.query.searchTitle ?? "";
+
+      return collection
+        .find(
+          {
+            termCode,
+            $and: [
+              { courseCode: { $regex: searchCode, $options: "i" } },
+              { courseTitle: { $regex: searchTitle, $options: "i" } },
+            ],
+          },
+          {
+            projection: { _id: 0 },
+          },
+        )
         .toArray();
     },
   );
-
-  async function latestTermCode(): Promise<string> {
-    const codes = await fastify.mongo
-      .db!.collection("course-catalog")
-      .distinct<string>("termCode", {});
-    return String(Math.max(...codes.map((it) => Number(it))));
-  }
 
   // Get course by courseID; if termCode is not specified, use the latest
   fastify.get(
@@ -60,10 +78,10 @@ const courseCatalog: FastifyPluginAsync = async (
     async function (request) {
       const courseID = request.params.courseID;
       const termCode = request.query.termCode ?? (await latestTermCode());
-      console.log(termCode);
-      const result = await fastify.mongo
-        .db!.collection("course-catalog")
-        .findOne<Course>({ termCode, courseID }, { projection: { _id: 0 } });
+      const result = await collection.findOne(
+        { termCode, courseID },
+        { projection: { _id: 0 } },
+      );
       if (result) {
         return result;
       } else {
@@ -95,9 +113,10 @@ const courseCatalog: FastifyPluginAsync = async (
     async function (request) {
       const courseCode = request.params.courseCode;
       const termCode = request.query.termCode ?? (await latestTermCode());
-      const result = await fastify.mongo
-        .db!.collection("course-catalog")
-        .findOne<Course>({ termCode, courseCode }, { projection: { _id: 0 } });
+      const result = await collection.findOne(
+        { termCode, courseCode },
+        { projection: { _id: 0 } },
+      );
       if (result) {
         return result;
       } else {
@@ -128,9 +147,8 @@ const courseCatalog: FastifyPluginAsync = async (
     async function (request) {
       const coursePrefix = request.params.coursePrefix;
       const termCode = request.query.termCode ?? (await latestTermCode());
-      return await fastify.mongo
-        .db!.collection("course-catalog")
-        .find<Course>({ termCode, coursePrefix }, { projection: { _id: 0 } })
+      return await collection
+        .find({ termCode, coursePrefix }, { projection: { _id: 0 } })
         .toArray();
     },
   );
