c5baec5b5e823801425e67e7014c61f2466ba768
Flandia Yingman <me@flandia.dev>
2025-02-09 21:35:18 +0800

    feat(announcements): support announcements

---
+++ src/routes/v1/announcements/index.ts
@@ -0,0 +1,46 @@
+import { FastifyTypebox } from "../../../app.js";
+import { Static, Type } from "@sinclair/typebox";
+import { FastifyPluginAsync } from "fastify";
+
+const Announcement = Type.Object({
+  id: Type.String(),
+  title: Type.String(),
+  content: Type.String(),
+  showBefore: Type.Optional(Type.String({ format: "date-time" })),
+  showAfter: Type.Optional(Type.String({ format: "date-time" })),
+});
+
+type Announcement = Static<typeof Announcement>;
+
+const announcement: FastifyPluginAsync = async (
+  fastify: FastifyTypebox,
+): Promise<void> => {
+  const collection =
+    fastify.mongo.db!.collection<Announcement>("announcements");
+  fastify.get(
+    "/",
+    {
+      schema: {
+        summary: "Get Announcements",
+        tags: ["Announcement"],
+        response: {
+          200: Type.Array(Announcement),
+        },
+      },
+    },
+    async function () {
+      return await collection
+        .aggregate<Announcement>([
+          {
+            $addFields: {
+              _id: "$$REMOVE",
+              id: "$_id",
+            },
+          },
+        ])
+        .toArray();
+    },
+  );
+};
+
+export default announcement;
