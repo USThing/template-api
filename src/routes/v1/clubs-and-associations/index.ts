7b60b5fc7ea00c8814724adf17fcab2e36b96006
Chow Chung Yan <cychowaq@connect.ust.hk>
2025-09-16 22:21:53 +0800

    fix(clubs): format to pass lint

--- src/routes/v1/clubs-and-associations/index.ts
+++ src/routes/v1/clubs-and-associations/index.ts
@@ -1,104 +1,111 @@
 import { FastifyTypebox } from "../../../app.js";
-
 import { ClubInfoSchema, ClubInfo, ClubInfoWithImageSchema } from "./types.js";
 import { Type } from "@sinclair/typebox";
 import { FastifyPluginAsync } from "fastify";
-import fs from 'fs';
-import { GridFSBucket } from 'mongodb';
+import fs from "fs";
+import { GridFSBucket } from "mongodb";
+import type { Db } from "mongodb";
 
 const clubsInfo: FastifyPluginAsync = async (
   fastify: FastifyTypebox,
 ): Promise<void> => {
   const collection = fastify.mongo.db!.collection<ClubInfo>("clubs");
 
-  async function uploadImage(filePath: string, fileName:string, db: any) {
-    const bucket = new GridFSBucket(db, { bucketName: 'images' });
+  async function uploadImage(filePath: string, fileName: string, db: Db) {
+    const bucket = new GridFSBucket(db, { bucketName: "images" });
 
     return new Promise((resolve, reject) => {
       const readStream = fs.createReadStream(filePath);
-      const uploadStream = bucket.openUploadStream(fileName, { contentType: 'image/png' });
-      readStream.on('error', reject);
-      uploadStream.on('error', reject);
-      uploadStream.on('finish', resolve);
+      const uploadStream = bucket.openUploadStream(fileName, {
+        contentType: "image/png",
+      });
+      readStream.on("error", reject);
+      uploadStream.on("error", reject);
+      uploadStream.on("finish", resolve);
       readStream.pipe(uploadStream);
     });
-  };
+  }
 
-  async function uploadClubs(data: ClubInfo[], db: any) {
-  const collection = db.collection('clubs');
-  const BASE_IMAGE_PATH = "assets/clubLogos/";
-  //first, validate all image paths
-  for (const club of data) {
-    try {
-      await fs.promises.access(`${BASE_IMAGE_PATH}${club.imageName}`);
-    } catch (err) {
-      throw new Error(`Logo path for club ${club.name} is invalid. Current path: ${BASE_IMAGE_PATH}${club.imageName}`);
+  async function uploadClubs(data: ClubInfo[], db: Db) {
+    const collection = db.collection("clubs");
+    const BASE_IMAGE_PATH = "assets/clubLogos/";
+    //first, validate all image paths
+    for (const club of data) {
+      try {
+        await fs.promises.access(`${BASE_IMAGE_PATH}${club.imageName}`);
+      } catch (err) {
+        fastify.log.error(err);
+        throw new Error(
+          `Logo path for club ${club.name} is invalid. Current path: ${BASE_IMAGE_PATH}${club.imageName}`,
+        );
+      }
+    }
+    for (const club of data) {
+      await uploadImage(
+        `${BASE_IMAGE_PATH}${club.imageName}`,
+        club.imageName,
+        db,
+      );
+      await collection.updateOne(
+        { name: club.name },
+        { $set: club },
+        { upsert: true },
+      );
     }
   }
-  for (let club of data) {
-    await uploadImage(`${BASE_IMAGE_PATH}${club.imageName}`, club.imageName, db);
-    await collection.updateOne(
-      { name: club.name },
-      { $set: club },
-      { upsert: true }
-    );
 
-  }
-}
+  async function getBase64FromFilename(filename: string): Promise<string> {
+    const db = fastify.mongo.db;
+    if (!db) {
+      throw new Error("No database connection");
+    }
+    const bucket = new GridFSBucket(db, { bucketName: "images" });
 
+    return new Promise((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      const downloadStream = bucket.openDownloadStreamByName(filename);
 
-async function getBase64FromFilename(filename: string): Promise<string> {
-const db = fastify.mongo.db;
-if (!db) {
-    throw new Error("No database connection");
-}
-  const bucket = new GridFSBucket(db, { bucketName: 'images' });
-
-  return new Promise((resolve, reject) => {
-    const chunks: Buffer[] = [];
-    const downloadStream = bucket.openDownloadStreamByName(filename);
-
-    downloadStream.on('data', chunk => chunks.push(chunk));
-    downloadStream.on('error', err => reject(err));
-    downloadStream.on('end', () => {
-      const buffer = Buffer.concat(chunks);
-      const base64 = buffer.toString('base64');
-      resolve(base64);
+      downloadStream.on("data", (chunk) => chunks.push(chunk));
+      downloadStream.on("error", (err) => reject(err));
+      downloadStream.on("end", () => {
+        const buffer = Buffer.concat(chunks);
+        const base64 = buffer.toString("base64");
+        resolve(base64);
+      });
     });
-  });
-}
+  }
 
-async function deleteClubs(names: string[], db: any) {
-    const collection = db.collection('clubs');
-    const bucket = new GridFSBucket(db, { bucketName: 'images' });
+  async function deleteClubs(names: string[], db: Db) {
+    const collection = db.collection("clubs");
+    const bucket = new GridFSBucket(db, { bucketName: "images" });
 
     for (const name of names) {
-        const club = await collection.find({ "name": name }).toArray();
-        if (club.length > 0) {
-            for (const c of club) {
-                if (c.logoPath) {
-                    // Delete image from GridFS
-                    const files = await bucket.find({ filename: c.logoPath }).toArray();
-                    for (const file of files) {
-                        await bucket.delete(file._id);
-                    }
-                }
+      const club = await collection.find({ name: name }).toArray();
+      if (club.length > 0) {
+        for (const c of club) {
+          if (c.logoPath) {
+            // Delete image from GridFS
+            const files = await bucket.find({ filename: c.logoPath }).toArray();
+            for (const file of files) {
+              await bucket.delete(file._id);
             }
-        collection.deleteMany({ "name": name });
+          }
         }
+        collection.deleteMany({ name: name });
+      }
     }
-}
+  }
 
   // Get all clubs, optionally filtered by a search string of name
 
-  
   fastify.get(
     "/clubs",
     {
       schema: {
         summary:
           "Get all clubs, optionally filtered by a search string of name.",
-        description: "If no search string is specified, all clubs will be returned.",
+        description:
+          "If no search string is specified, all clubs will be returned.",
         tags: ["Clubs"],
         querystring: Type.Object({
           searchName: Type.Optional(Type.String()),
@@ -110,12 +117,10 @@ async function deleteClubs(names: string[], db: any) {
     },
     async function (request) {
       const searchName = request.query.searchName ?? "";
-      let searchResults = await collection
+      const searchResults = await collection
         .find(
           {
-            $or: [
-              { name: { $regex: searchName, $options: "i" } },
-            ],
+            $or: [{ name: { $regex: searchName, $options: "i" } }],
           },
           {
             projection: { _id: 0 },
@@ -123,26 +128,26 @@ async function deleteClubs(names: string[], db: any) {
         )
         .toArray();
 
-    const finalResults = await Promise.all(
-      searchResults.map(async (club) => {
-        if (club.imageName) {
-          try {
-            const base64 = await getBase64FromFilename(club.imageName);
-            return { ...club, imageBase64: base64 };
-          } catch (error) {
-            fastify.log.error(error);
-            return { ...club, imageBase64: "" };
+      const finalResults = await Promise.all(
+        searchResults.map(async (club) => {
+          if (club.imageName) {
+            try {
+              const base64 = await getBase64FromFilename(club.imageName);
+              return { ...club, imageBase64: base64 };
+            } catch (error) {
+              fastify.log.error(error);
+              return { ...club, imageBase64: "" };
+            }
           }
-        }
-        return { ...club, imageBase64: "" };
-      })
-    );
+          return { ...club, imageBase64: "" };
+        }),
+      );
 
-    return finalResults;
-  }
-);
+      return finalResults;
+    },
+  );
 
- fastify.get(
+  fastify.get(
     "/clubNames",
     {
       schema: {
@@ -157,76 +162,79 @@ async function deleteClubs(names: string[], db: any) {
     async function (request, reply) {
       const clubNames = await collection.distinct("name");
       reply.send(clubNames);
-    }
+    },
   );
 
   fastify.post(
-    "/uploadClubs", 
+    "/uploadClubs",
     {
-        schema: {
-          summary: "Upload or update club information",
-          description: "Upload or update club information. If a club with the same name exists, it will be updated.",
-          tags: ["Clubs"],
-            body: Type.Array(ClubInfoSchema),
-            response: {
-                201: Type.Object({
-                    success: Type.Boolean(),
-                    message: Type.String(),
-                }),
-            },
+      schema: {
+        summary: "Upload or update club information",
+        description:
+          "Upload or update club information. If a club with the same name exists, it will be updated.",
+        tags: ["Clubs"],
+        body: Type.Array(ClubInfoSchema),
+        response: {
+          201: Type.Object({
+            success: Type.Boolean(),
+            message: Type.String(),
+          }),
         },
+      },
     },
     async function (request, reply) {
-        const clubs = request.body;
-        await uploadClubs(clubs, fastify.mongo.db!);
+      const clubs = request.body;
+      await uploadClubs(clubs, fastify.mongo.db!);
 
-        reply.status(201).send({
-            success: true,
-            message: "Club uploaded successfully",
-        });
-    }
-)
+      reply.status(201).send({
+        success: true,
+        message: "Club uploaded successfully",
+      });
+    },
+  );
 
-fastify.post("/deleteClubs", {
-    schema: {
+  fastify.post(
+    "/deleteClubs",
+    {
+      schema: {
         summary: "Delete clubs by names",
-        description: "Delete clubs by providing an array of club names. This will also delete associated images from GridFS.",
+        description:
+          "Delete clubs by providing an array of club names. This will also delete associated images from GridFS.",
         tags: ["Clubs"],
         body: Type.Object({
-            names: Type.Array(Type.String(), {
-                description: 'Array of club names to be deleted',
-                examples: [['Fencing Club', 'The Catholic Society']]
-            })
+          names: Type.Array(Type.String(), {
+            description: "Array of club names to be deleted",
+            examples: [["Fencing Club", "The Catholic Society"]],
+          }),
         }),
         response: {
-            200: Type.Object({
-                success: Type.Boolean(),
-                message: Type.String(),
-            }),
+          200: Type.Object({
+            success: Type.Boolean(),
+            message: Type.String(),
+          }),
         },
-    }},
+      },
+    },
     async function (request, reply) {
-        const { names } = request.body;
-        for (const name of names) {
-            const club = await collection.find({ "name": name }).toArray();
-            if (club.length === 0) {
-                reply.status(404).send({
-                    success: false,
-                    message: `Club with name ${name} not found`,
-                });
-                return;
-            }
+      const { names } = request.body;
+      for (const name of names) {
+        const club = await collection.find({ name: name }).toArray();
+        if (club.length === 0) {
+          reply.status(404).send({
+            success: false,
+            message: `Club with name ${name} not found`,
+          });
+          return;
         }
-        await deleteClubs(names, fastify.mongo.db!);
+      }
+      await deleteClubs(names, fastify.mongo.db!);
 
-        reply.status(200).send({
-            success: true,
-            message: "Clubs deleted successfully",
-        });
-    }
-);
-
-
-}
+      reply.status(200).send({
+        success: true,
+        message: "Clubs deleted successfully",
+      });
+    },
+  );
+};
 
 export default clubsInfo;
--- src/routes/v1/clubs-and-associations/types.ts
+++ src/routes/v1/clubs-and-associations/types.ts
@@ -1,5 +1,5 @@
-import { Type } from '@sinclair/typebox'
-import { Static } from '@sinclair/typebox'
+import { Type } from "@sinclair/typebox";
+import { Static } from "@sinclair/typebox";
 
 // Enums
 export enum ClubCategory {
@@ -32,99 +32,107 @@ export enum SocialMediaType {
   WECHAT,
 }
 
-
-const allowedSocialMediaTypes = Object.keys(SocialMediaType).filter(k => isNaN(Number(k)));
-
-export const SocialMediaTypeSchema = Type.Union(
-  allowedSocialMediaTypes.map(type => Type.Literal(type))
+const allowedSocialMediaTypes = Object.keys(SocialMediaType).filter((k) =>
+  isNaN(Number(k)),
 );
 
-const allowedClubCategories = Object.keys(ClubCategory).filter(k => isNaN(Number(k)));
+export const SocialMediaTypeSchema = Type.Union(
+  allowedSocialMediaTypes.map((type) => Type.Literal(type)),
+);
+
+const allowedClubCategories = Object.keys(ClubCategory).filter((k) =>
+  isNaN(Number(k)),
+);
 
 export const ClubCategorySchema = Type.Union(
-  allowedClubCategories.map(category => Type.Literal(category))
+  allowedClubCategories.map((category) => Type.Literal(category)),
 );
 
 export const MembershipFeeInfoSchema = Type.Object({
   oneYear: Type.String({
-    description: 'Fee for one year membership, in HKD. ',
-    examples: ['HKD 0', 'HKD 50', 'TBA', "Not Available"],
+    description: "Fee for one year membership, in HKD. ",
+    examples: ["HKD 0", "HKD 50", "TBA", "Not Available"],
   }),
   twoYear: Type.String({
-    description: 'Fee for two year membership, in HKD.',
-    examples: ['HKD 0','HKD 90', 'TBA', "Not Available"],
+    description: "Fee for two year membership, in HKD.",
+    examples: ["HKD 0", "HKD 90", "TBA", "Not Available"],
   }),
   threeYear: Type.String({
-    description: 'Fee for three year membership, in HKD. ',
-    examples: ['HKD 0','HKD 120', 'TBA', "Not Available"],
+    description: "Fee for three year membership, in HKD. ",
+    examples: ["HKD 0", "HKD 120", "TBA", "Not Available"],
   }),
   fourYear: Type.String({
-    description: 'Fee for four year membership, in HKD. ',
-    examples: ['HKD 0','HKD 150', 'TBA', "Not Available"],
+    description: "Fee for four year membership, in HKD. ",
+    examples: ["HKD 0", "HKD 150", "TBA", "Not Available"],
   }),
-})
+});
 
 export const SocialMediaInfoSchema = Type.Object({
   type: Type.String(SocialMediaTypeSchema),
   text: Type.String({
-    description: 'The display text for the link',
-    examples: ['@oceanscience_hkust', 'The Business Students\' Union LINKEDIN']
+    description: "The display text for the link",
+    examples: ["@oceanscience_hkust", "The Business Students' Union LINKEDIN"],
   }),
   link: Type.String({
-    description: 'The actual URL or link',
-    examples: ['https://forms.gle/2AuUSNX3jkzHf8Z99', "https://www.linkedin.com/company/the-business-students'%E2%80%8B-union-hkustsu/"]
+    description: "The actual URL or link",
+    examples: [
+      "https://forms.gle/2AuUSNX3jkzHf8Z99",
+      "https://www.linkedin.com/company/the-business-students'%E2%80%8B-union-hkustsu/",
+    ],
   }),
-})
+});
 
 export const ContactInfoSchema = Type.Object({
   name: Type.String({
-    description: 'Name of the contact person',
-    examples: ['CHAN, Tai Man'],
+    description: "Name of the contact person",
+    examples: ["CHAN, Tai Man"],
   }),
   telephone: Type.String({
-    description: 'Contact telephone number',
-    examples: ['12345678', 'Not Available'],
+    description: "Contact telephone number",
+    examples: ["12345678", "Not Available"],
   }),
   email: Type.String({
-    description: 'Contact email address',
-    examples: ['su_drama@connect.ust.hk'],
+    description: "Contact email address",
+    examples: ["su_drama@connect.ust.hk"],
   }),
-})
+});
 
 export const ClubInfoSchema = Type.Object({
   name: Type.String({
-    description: 'Full name of the club or association',
-    examples: ['Fencing Club'],
+    description: "Full name of the club or association",
+    examples: ["Fencing Club"],
   }),
   categories: Type.Array(Type.String(ClubCategorySchema)),
   department: Type.String({
-    description: 'Affiliation type of the club.',
-    examples: ['Department Associated Group', 'HKUSTSU']
+    description: "Affiliation type of the club.",
+    examples: ["Department Associated Group", "HKUSTSU"],
   }),
   description: Type.String({
-    description: 'Description of the club', 
-    examples:['University Philharmonic Orchestra (UPO) is a student-run orchestra under the Student Union of The Hong Kong University of Science and Technology (HKUSTSU). \n\nThe orchestra was previously known as the HKUSTSU Wind Ensemble, which merged with the Strings Ensemble in 2005. The orchestra consists of musicians from different backgrounds, including local, international, exchange students, postgraduate researchers, alumni, and friends of HKUST. \n\nUPO aims to provide a platform for music-loving HKUST members to create and enjoy music. We also hope to nourish our musicians\' musicality and skills through playing orchestral music. In the past, we were invited to perform at functions such as the Outreach Day in HKUST, independent clubs\' orientation camps, and inauguration ceremonies. In recent years, UPO has given concerts at the Shaw Auditorium at HKUST.']
+    description: "Description of the club",
+    examples: [
+      "University Philharmonic Orchestra (UPO) is a student-run orchestra under the Student Union of The Hong Kong University of Science and Technology (HKUSTSU). \n\nThe orchestra was previously known as the HKUSTSU Wind Ensemble, which merged with the Strings Ensemble in 2005. The orchestra consists of musicians from different backgrounds, including local, international, exchange students, postgraduate researchers, alumni, and friends of HKUST. \n\nUPO aims to provide a platform for music-loving HKUST members to create and enjoy music. We also hope to nourish our musicians' musicality and skills through playing orchestral music. In the past, we were invited to perform at functions such as the Outreach Day in HKUST, independent clubs' orientation camps, and inauguration ceremonies. In recent years, UPO has given concerts at the Shaw Auditorium at HKUST.",
+    ],
   }),
   links: Type.Array(SocialMediaInfoSchema),
   contacts: Type.Array(ContactInfoSchema),
   membershipFees: Type.Optional(MembershipFeeInfoSchema),
   imageName: Type.String({
-    description: 'File name of club logo',
-    examples: ['ASCE.png']
-  })
-})
+    description: "File name of club logo",
+    examples: ["ASCE.png"],
+  }),
+});
 
 export const ClubInfoWithImageSchema = Type.Intersect([
   ClubInfoSchema,
-  Type.Object({ 
+  Type.Object({
     imageBase64: Type.String({
-      description: 'Base64 encoded image data',
-      examples: ['iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAYAAAA+5n6JAAAABmJLR0QA/wD/AP+gvaeTAAAX...'],
-    })
-  })
+      description: "Base64 encoded image data",
+      examples: [
+        "iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAYAAAA+5n6JAAAABmJLR0QA/wD/AP+gvaeTAAAX...",
+      ],
+    }),
+  }),
 ]);
 
 export type ClubInfo = Static<typeof ClubInfoSchema>;
 export type ClubInfoWithImage = Static<typeof ClubInfoWithImageSchema>;
-
-
