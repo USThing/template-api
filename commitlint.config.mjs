161ee3782d0c7f1b532521910df7269bc69ce3cb
William So <polyipseity@gmail.com>
2025-11-02 08:52:59 +0800

    build: disable commitlint `header-max-length`

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,9 @@
+import { RuleConfigSeverity } from "@commitlint/types";
+
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Dependabot or we like long headers...
+    "header-max-length": [RuleConfigSeverity.Disabled, "always", 0],
+  },
 };
