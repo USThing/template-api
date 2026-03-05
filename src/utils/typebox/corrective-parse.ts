eaa900c51f6b30a22a32384bb9048945d4d5e281
Flandia Yingman <me@flandia.dev>
2026-03-05 03:02:40 -0500

    fix my own issue

--- src/utils/typebox/corrective-parse.ts
+++ src/utils/typebox/corrective-parse.ts
@@ -1,18 +1,17 @@
-import Type from "typebox";
+import Type, { Static } from "typebox";
 import Value, { Pipeline } from "typebox/value";
 
 const CorrectiveParsePipeline = Pipeline([
-  // Comment and re-order as necessary
   (_context, _type, value) => Value.Clone(value),
   (context, type, value) => Value.Default(context, type, value),
   (context, type, value) => Value.Convert(context, type, value),
   (context, type, value) => Value.Clean(context, type, value),
-  // Run Parse as final operation
   (context, type, value) => Value.Parse(context, type, value),
 ]);
+
 export default function CorrectiveParse<T extends Type.TSchema>(
   type: T,
   value: unknown,
-): ReturnType<typeof Value.Parse<T>> {
+): Static<T> {
   return CorrectiveParsePipeline(type, value) as never;
 }
